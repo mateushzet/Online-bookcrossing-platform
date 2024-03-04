68ebcc272399a5a4c1de0b6bf9f73f9baddad0b1
Mateusz Zięcina <ziecinamat+github@gmail.com>
2024-03-04 23:19:25 +0100

    Initial commit

---
+++ src/main/java/com/bookcrossing/springboot/BookCrossingApplication.java
@@ -0,0 +1,13 @@
+package com.bookcrossing.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookCrossingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookCrossingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookcrossing/springboot/BookCrossingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookcrossing.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookCrossingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
